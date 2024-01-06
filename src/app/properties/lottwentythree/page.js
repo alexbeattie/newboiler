41a782fe4b90391f2a905dbc91b39918f6c1a8de
Alex Beattie <artisanbranding@me.com>
2024-01-06 08:31:44 -0800

    added lot 23 images - changed text

--- src/app/properties/lottwentythree/page.js
+++ src/app/properties/lottwentythree/page.js
@@ -10,8 +10,8 @@ export default function LotTwentyThree() {
 
       <Image
         className=""
-        src="https://alexbeattie.s3.amazonaws.com/lot-19.jpg"
-        alt="Lot Two"
+        src="https://alexbeattie.s3.amazonaws.com/23.jpg"
+        alt="Lot Twenty Three"
         width={2560}
         height={1440}
         backgroundSize="cover"
@@ -20,22 +20,22 @@ export default function LotTwentyThree() {
       />
       <article className='px-6 py-12 shadow-xl bg-white p-2 prose prose-slate mx-auto mt-8 lg:prose-lg'>
 
-        <h2>Lot Nineteen - Sold - Fifteen Acres</h2>
+        <h2>Lot Twenty-Three - Seventeen Acres</h2>
 
-        <p className='lead'>118 Skywater Hollow, Salt Spring Island, BC</p>
+        <p>554 Skywater Drive, Salt Spring Island, BC</p>
 
-        <p>The large building site borders an 80 acre Park Reserve to the north offering hiking and walking trails for your pleasure. The easterly view includes potential vistas of your wetlands; other Skywater properties; and Ocean Views including the Southern Gulf Islands, the San Juan Islands and Mt. Baker</p>
+        <p>The driveway to this property leaves Skywater Drive heading north through the valley between two wetlands up a southerly slope passing between two large rock outcrops to the recommended building site that overlooks a year round pond.</p>
 
-        <p>Access to this large diverse property is off Skywater Hollow that passes between two spectacular wetlands, meanders through a treed area curving left up a gradual ravine to the building site.</p>
+        <p>This parcel of land commands your attention... sharing a 1/4 mile boundary with an expansive little used 80 acre park reserve, offering hiking/walking trails for your pleasure and total privacy. The park reserve is the area the Property Legend is shown on the Property Map.</p>
 
-        <p>Lot Nineteen has it all…ocean views, wetlands, vistas, forested areas, borders park reserve; and the real potential to be developed in a small farm with gardens, orchards, meadows, crops, pastures...or whatever other agricultural ventures you may desire…sheep…horses…cattle.</p>
+        <p>The land is partially treed with the main slope facing south west for maximum sunlight. Lot 23 could be developed into a very private retreat to enjoy with an elevated building site overlooking the year round pond and magnificent terrain ... trails, paths and waterways meander across the property and the 80 acre park reserve among large cedar and fir trees and much more, for your pleasure.</p>
 
-        <p>The developer would be happy to offer it services to further develop this property to suit the owner’s preferences at very reasonable rates.</p>
+        <p>Lot 23 also enjoys the exclusive use of the Limited Common Property shown on the Property in yellow. This property is owned by the Skywater Strata but its exclusive use is limited to the registered owner of Lot 23. The smaller, approximately one acre parcel across from Lot 1 was used by the Developer for gravel and sandstone boulder storage. There is some gravel left and many sandstone boulders. The larger, approximately seven acre, parcel on the other side of Musgrave Road is well treed with Fir, Cedar and some open spaces. </p>
 
-        <p>Zoning allows for Main Home; Guest Cottage; Accessory Building(s); and Agriculture.</p>
 
-        <p>Private driveway to building site; drilled water well at building site; power and communication services to the property line.</p>
+        <p>Zoning allows for Main House, Guest Cottage, Accessory Building(s) and Agriculture.</p>
 
+        <p>Private Driveway to building site; drilled water well near building site; power and communication services to the property line.</p>
 
 
 
